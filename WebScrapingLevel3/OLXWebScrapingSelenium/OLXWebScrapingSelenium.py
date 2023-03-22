956962d325acde225058f41cccf5baf232d8f3fa
MarcoAntonioGarcia <marco.antonio.06@outlook.com>
2023-03-22 11:32:29 -0600

    first programm with selenium where we get information of OLX, we use selenium's tools and implement files .ipynb and .py

---
+++ WebScrapingLevel3/OLXWebScrapingSelenium/OLXWebScrapingSelenium.py
@@ -0,0 +1,43 @@
+import random
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome('./chromedriver') #with that we get the control of the chrome page
+#driver.get('https://www.olx.com.ar/autos_c378') #open the page where we need get the information
+driver.get('https://www.olx.com.ar/')
+
+for i in range (3):
+    try:
+        
+        button=WebDriverWait(driver, 10).until(
+            #it going to get the event espected
+            #in this case it take the time when the button is recharged
+            EC.presence_of_element_located((By.XPATH, '//button[@data-aut-id="btnLoadMore"]'))
+        )
+        #we need rewrite the xpath to looking for the button because each time in the new page, it change but with the same path
+        #button = driver.find_element("xpath",'//button[@data-aut-id="btnLoadMore"]') ya no lo necesito porque ya se consulto 
+        button.click()
+        #sleep(random.uniform(8.0,10.0)) #Random time is less eficient, it's why we going to use WebDirverWait
+
+        WebDriverWait(driver, 10).until(
+            #it going to get the event espected
+            #in this case it take the time when the button is recharged
+            EC.presence_of_all_elements_located((By.XPATH, '//li[@data-aut-id="itemBox"]//span[@data-aut-id="itemPrice"]'))
+        )
+
+    except:
+        #we are using this because sometimes over the buton we will find some objets and the click is imposible
+        #or only we will get a error in click and with that we going to break this click and finish the program
+        break 
+    
+autos=driver.find_elements("xpath",'//li[@data-aut-id="itemBox"]')
+
+for auto in autos:
+    precio = auto.find_element("xpath", './/span[@data-aut-id="itemPrice"]').text
+    print(precio)
+    description = auto.find_element("xpath",'.//span[@data-aut-id="itemTitle"]').text
+    print(description)
+
