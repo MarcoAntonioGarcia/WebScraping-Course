8d487a0272df18b7f4ea05d235db069dbecbeb47
MarcoAntonioGarcia <marco.antonio.06@outlook.com>
2023-03-23 18:45:03 -0600

    I have finished the Web Scraping in OLX and I going to added the final program in .ipynb and .py

--- WebScrapingLevel3/OLXWebScrapingSelenium/OLXWebScrapingSelenium.py
+++ WebScrapingLevel3/OLXWebScrapingSelenium/OLXWebScrapingSelenium.py
@@ -4,15 +4,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service #Extra importation because we have deprecated methods
 
-driver = webdriver.Chrome('./chromedriver') #with that we get the control of the chrome page
-#driver.get('https://www.olx.com.ar/autos_c378') #open the page where we need get the information
-driver.get('https://www.olx.com.ar/')
+servicio=Service('./chromedriver')
+driver = webdriver.Chrome(service=servicio) #with that we get the control of the chrome page
+driver.get('https://www.olx.com.ar/autos_c378') #open the page where we need get the information
+
 
 for i in range (3):
     try:
         
-        button=WebDriverWait(driver, 10).until(
+        button=WebDriverWait(driver, 200).until(
             #it going to get the event espected
             #in this case it take the time when the button is recharged
             EC.presence_of_element_located((By.XPATH, '//button[@data-aut-id="btnLoadMore"]'))
@@ -22,7 +24,7 @@ for i in range (3):
         button.click()
         #sleep(random.uniform(8.0,10.0)) #Random time is less eficient, it's why we going to use WebDirverWait
 
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 200).until(
             #it going to get the event espected
             #in this case it take the time when the button is recharged
             EC.presence_of_all_elements_located((By.XPATH, '//li[@data-aut-id="itemBox"]//span[@data-aut-id="itemPrice"]'))
@@ -31,6 +33,7 @@ for i in range (3):
     except:
         #we are using this because sometimes over the buton we will find some objets and the click is imposible
         #or only we will get a error in click and with that we going to break this click and finish the program
+        print("ERROR")
         break 
     
 autos=driver.find_elements("xpath",'//li[@data-aut-id="itemBox"]')
@@ -40,4 +43,5 @@ for auto in autos:
     print(precio)
     description = auto.find_element("xpath",'.//span[@data-aut-id="itemTitle"]').text
     print(description)
+print(len(autos))    
 
